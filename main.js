b3c05d86487369052b36ab977bfbf88b70130505
Elber <elber.nava17@gmail.com>
2021-10-24 11:18:15 -0400

    finished

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+  const url = 'http://127.0.0.1:5500/page/index.html';
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('http://127.0.0.1:5500/page/index.html', {
+  await page.goto(url, {
     waitUntil: 'networkidle2',
   });
-  await page.pdf({ path: 'elber-nava-2021.pdf', format: 'a4' });
+  await page.pdf({ path: 'curriculum.pdf', format: 'a4' });
 
   await browser.close();
 })();
