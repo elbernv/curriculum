823db2a5a91612de8ff4f6806da8a2bb2267ebdb
elbernv <elbernava11@gmail.com>
2023-02-06 18:29:49 -0400

    updated

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  serverUrl: "http://127.0.0.1:3000",
+  frontUrl: "http://127.0.0.1:5050",
+};
+
+try {
+  module.exports = config;
+} catch (error) {}
--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-const puppeteer = require('puppeteer');
+const path = require("path");
+const puppeteer = require("puppeteer");
+const express = require("express");
+const app = express();
+const config = require("./config");
+const port = 3000;
 
-(async () => {
-  const url = 'http://127.0.0.1:5500/page/index.html';
+app.get("/download-pdf", async (req, res) => {
+  const pdfName = "Elber-Nava-2023.pdf";
+  const url = `${config.frontUrl}/index.html?download=true`;
+  console.log(url);
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url, {
-    waitUntil: 'networkidle2',
+    waitUntil: "networkidle2",
   });
-  await page.pdf({ path: 'curriculum.pdf', format: 'a4' });
+  await page.pdf({ path: pdfName, format: "a4" });
 
   await browser.close();
-})();
+
+  res.contentType("application/pdf");
+  res.set("Content-Disposition", `attachment; filename="${pdfName}"`);
+  res.sendFile(path.join(process.cwd(), pdfName));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
